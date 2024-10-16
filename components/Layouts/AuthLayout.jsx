e21a47b8125f92146dedc55a87d6115b8d861f51
Jose Javier <ing.josemartinezmendez@gmail.com>
2024-10-16 10:02:37 -0600

    change

--- components/Layouts/AuthLayout.jsx
+++ components/Layouts/AuthLayout.jsx
@@ -1,25 +1,10 @@
 import { redirect } from "next/navigation"
 import { cookies } from "next/headers"
-//components
+
 import Navbar from "../Navbar"
 import Sidebar from "../Sidebar"
 
-<<<<<<< HEAD
-export default function AuthLayout({children}) {
-    return (
-      <div className="h-screen w-full flex">
-        <Sidebar />
-        <div className="flex flex-col w-full">
-          <Navbar />
-          <main className="w-full flex-grow p-4">
-            {children}
-          </main>
-        </div>
-      </div>
-    )
-  } 
-=======
-function AuthLayout({ children }) {
+export default function AuthLayout({ children }) {
 
   const token = cookies().get('token')
   if (!token) {
@@ -38,6 +23,3 @@ function AuthLayout({ children }) {
     </div>
   )
 }
-
-export default AuthLayout
->>>>>>> e00f6fb26ef2fdacde949e2e60427b60674521eb
